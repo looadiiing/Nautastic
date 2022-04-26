44e317ed8037d7eaea4c490615edd2c3e39c741d
looadiiing <ulpenn@gmail.com>
2022-04-26 15:16:31 +0200

    Initial version of Nauticastic generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/zplcompany/nauticastic/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.zplcompany.nauticastic.security;
---
+++ src/main/java/com/zplcompany/nauticastic/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.zplcompany.nauticastic.service.dto;
